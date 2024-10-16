32ddf439633a3a3371accd9959e49d660db63897
iamsyahirah <syahirahmohammad89@gmail.com>
2024-10-17 02:04:57 +0800

    feat: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { CustomerReview, Footer, Hero, PopularProducts, Services, SpecialOffer, SuperQuality, Subscribe } from "./sections";
-
+import Nav from "./components/Nav";
 const App = () => {
   return (
     <main className="relative">
+      <Nav />
       <section className="xl:padding-1 wide:padding-r padding-b">
         <Hero />
       </section>
@@ -16,10 +17,10 @@ const App = () => {
         <Services />
       </section>
       <section className="padding">
-        <SpecialOffers />
+        <SpecialOffer />
       </section>
       <section className="bg-pale-blue padding">
-        <CustomerReviews />
+        <CustomerReview />
       </section>
       <section className="padding-x sm:py-32 py-16 w-full">
         <Subscribe />
